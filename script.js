a2e1068a21d25c56a873299c8d87338cc56606a4
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-08-02 21:41:17 +0200

    Adding random quotes (generate and display)

--- script.js
+++ script.js
@@ -1,97 +1,28 @@
-// CONNEXION A L'API (SANS AUTHORIZATION)
-const options = {
-    method: 'GET',
-    headers: {
-        Accept: 'application/json',
-    }
+const citationsArray = [
+    `"La lecture commencer les yeux fermés" - Yvon Rivard`,
+    `“Une lecture amusante est aussi utile à la santé que l’exercice du corps.” - Emmanuel Kant`,
+    `“Une heure de lecture est le souverain remède contre les dégoûts de la vie.” - Montesquieu`,
+    `“La lecture est à l'esprit ce que l'exercice est au corps.” - J. Addison`,
+    `“Lire haut, c'est s'affirmer à soi-même sa lecture.” - Victor Hugo`,
+    `“Je déclare qu'après tout, il n'y a pas de plaisir qui vaille la lecture !” - Jane Austen`,
+    `“La vraie lecture commence quand on ne lit plus seulement pour se distraire et se fuir, mais pour se trouver.” - Jean Guéhenno`,
+    `“La lecture est pour moi le contraire de l'écriture. Je n'ai pas de rites : c'est pour me détendre, rire, pleurer, rêver.” - Nathalie Rheims`,
+    `“Ne plus lire depuis longtemps, c’est comme perdre un ami important.” - Proverbe chinois`,
+    `“Dans la lecture solitaire, l'homme qui se cherche lui-même a quelque chance de se rencontrer.” - Georges Duhamel`,
+    `“La lecture, une porte ouverte sur un monde enchanté.” - François Mauriac`,
+    `"La lecture est une amitié." - Marcel Proust`,
+    `“Le choix d’un bon livre n’est pas moins difficile que la lecture en est agréable.” - Chevalier de Méré`,
+    `“La lecture d’un roman jette sur la vie une lumière.” - Louis Aragon`,
+    `“Il n’y a vraiment que deux choses qui puissent faire changer un être humain : un grand amour ou la lecture d’un grand livre.” - Paul Desalmand`
+]
+
+function getRandomCitation() {
+    return citationsArray[Math.floor(Math.random() * citationsArray.length)];
 }
 
-const apiKey = 'AIzaSyAMkR99i88RXvSuDZ3CmZdbk5-zt8znpGE';
-
-// RECUPERATION DES ELEMENTS DU DOM
-const searchInput = document.getElementById('search-input');
-const dropdownContainer = document.getElementById('dropdown-container');
-
-
-// FONCTION PRINCIPALE
-async function searchBook() {
-    let requestString = `https://books.googleapis.com/books/v1/volumes?q=${searchInput.value}&printType=books&langRestrict=fr&key=${apiKey}`
-    let data = await fetch(requestString, options);
-    let response = await data.json();
-    
-    // if (dropdownContainer.innerHTML != null) {
-    //     dropdownContainer.innerHTML = null;
-    // }
-
-    // if (searchInput.length ===0) {
-    //     return
-    // }
-
-        response.items.forEach(book => { // Afficher la liste de résultats dans le dropdown container
-            const matchingResult = document.createElement('div');
-            matchingResult.classList.add('matching-result'); 
-            if (!book.volumeInfo.authors && !book.volumeInfo.publishedDate) {
-                matchingResult.innerHTML = `${book.volumeInfo.title})`;
-            } else if (!book.volumeInfo.authors) {
-                matchingResult.innerHTML = `${book.volumeInfo.title}<br>(${book.volumeInfo.publishedDate.slice(0,4)})`;
-            } else if (!book.volumeInfo.publishedDate) {
-                matchingResult.innerHTML = `${book.volumeInfo.title}<br><span>par ${book.volumeInfo.authors[0]}</span>`;
-            } else {
-                matchingResult.innerHTML = `${book.volumeInfo.title}<br><span>par ${book.volumeInfo.authors[0]}</span> (${book.volumeInfo.publishedDate.slice(0,4)})`;
-            }
-            dropdownContainer.appendChild(matchingResult);
-
-            if (response.length === 0){
-                noResults()
-            }
-        })
-
-} 
-
-
-// LANCEMENT DE LA FONCTION PRINCIPALE (RECHERCHE DANS L'API) A PARTIR D'UN INPUT UTILISATEUR
-searchInput.addEventListener('input', searchBook);
-
-
-
-// ADD EVENT LISTENER WHEN THE SEARCH BUTTON IS CLICKED 
-// const searchButton = document.getElementById('search-button');
-
-// searchButton.addEventListener("click", () => {
-
-// });
-
-
-
-// CLEANER LA LISTE 
-// function clearList(){ 
-//     // looping through each child of the search results list and remove each child
-//     while (list.firstChild){
-//         list.removeChild(list.firstChild);
-//     }
-// }
-
-// AFFICHER UN MESSAGE D'ERREUR SI PAS DE RESULTAT 
-function noResults(){
-    const error = document.createElement('li');
-    error.classList.add('error-message');
-
-    const text = document.createTextNode('No results found.');
-    error.appendChild(text);
-    list.appendChild(error);
+function displayRandomCitation() {
+    const citation = document.getElementById("citation");
+    citation.innerHTML = `${getRandomCitation()}`
 }
 
-
-// ADD EVENT LISTENER WHEN INPUT IS TYPED INTO THE INPUT FORM
-
-// searchInput.addEventListener('input', (e) => {
-//         if (searchInput && searchInput.trim().length > 2){ // 2. check if input exists and if input is larger than 2
-//             value = value.trim() // 3. redefine 'value' to exclude white space and change input to all lowercase
-//             setList(matchingBooks.filter(book => { 
-//                 // 4. returning only the results of setList() if the value of the search exists
-//                 return book.title.includes(value);
-//             }))
-//         } else {
-//             clearList()// 5. return nothing 
-//         }
-// });
+displayRandomCitation()
