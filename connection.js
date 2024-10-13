92244ffa28e0e034e9b62b40f86f464fa4eac2d0
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-10-13 13:12:45 +0200

    add: file for api connection

---
+++ connection.js
@@ -0,0 +1,9 @@
+// CONNEXION A L'API (SANS AUTHORIZATION)
+const options = {
+	method: "GET",
+	headers: {
+		Accept: "application/json",
+	},
+};
+
+const apiKey = "AIzaSyAMkR99i88RXvSuDZ3CmZdbk5-zt8znpGE";
