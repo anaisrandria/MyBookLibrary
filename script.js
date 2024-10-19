139781fdc51a8276626253eb32e0fb49773b2cdd
anaisrandria <anais.randriamitantsoa@gmail.com>
2024-10-19 12:16:15 +0200

    add: fallback image if missing thumbnail

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ async function fetchApi() {
     // console.log("🪲", response.items[0]);
     // console.log(`Titre: ${response.items[0].volumeInfo.title} + id: ${response.items[0].id}`);
 
+    // Afficher 5 nouveaux résultats à chaque fetch de l'api
     if (dropdownContainer.innerHTML != null) {
         dropdownContainer.innerHTML = null;
     }
@@ -25,25 +26,42 @@ async function fetchApi() {
 }; 
 
 
+// LANCEMENT DE LA FONCTION FETCH API A PARTIR D'UN INPUT UTILISATEUR
+let timer = 0;
+
+searchInput.addEventListener('input', () => {
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+        if (searchInput.value !== "") {
+            fetchApi();
+        } else {
+            dropdownContainer.innerHTML = "";
+        }
+    }, 500);
+});
+
+
 // ---------- AFFICHER DROPDOWN LIST ---------- //
 function displayResults(book, response) {
     const matchingResult = document.createElement("div");
     matchingResult.classList.add("matching-result");
     matchingResult.setAttribute("id", `${book.id}`);
     
+    const textContainer = document.createElement("div");
+    textContainer.classList.add("text-container");
+    
     const thumbnail = document.createElement("img");
     thumbnail.classList.add("thumbnail");
-    
     if (book.volumeInfo.imageLinks) {
         thumbnail.setAttribute("src", `${book.volumeInfo.imageLinks.thumbnail}`);
-    }
+    } else {
+        thumbnail.setAttribute("src", "assets/missingbook.jpg");
+    };
 
     const title = document.createElement("p");
-    const bookInformation = document.createElement("p");
-    const textContainer = document.createElement("div");
-    
-    title.innerHTML = `${book.volumeInfo.title}`;
+    title.innerHTML = book.volumeInfo.title;
 
+    const bookInformation = document.createElement("p");
     if (!book.volumeInfo.authors) {
         bookInformation.innerHTML = `<span>(${book.volumeInfo.publishedDate.slice(0, 4)})</span>`;
     } else if (!book.volumeInfo.publishedDate) {
@@ -52,12 +70,9 @@ function displayResults(book, response) {
         bookInformation.innerHTML = `<span>par ${book.volumeInfo.authors[0]} (${book.volumeInfo.publishedDate.slice(0, 4)})</span>`;
     }
  
-    if (book.volumeInfo.imageLinks) {
-        matchingResult.appendChild(thumbnail);
-    };
-
     textContainer.appendChild(title);
     textContainer.appendChild(bookInformation);
+    matchingResult.appendChild(thumbnail);
     matchingResult.appendChild(textContainer);
     dropdownContainer.appendChild(matchingResult);
 
@@ -68,42 +83,6 @@ function displayResults(book, response) {
 };
 
 
-// LANCEMENT DE LA FONCTION PRINCIPALE (RECHERCHE DANS L'API) A PARTIR D'UN INPUT UTILISATEUR
-
-let timer = 0;
-
-searchInput.addEventListener('input', () => {
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-        if (searchInput.value !== "") {
-            fetchApi();
-        }
-
-        if (searchInput.value == "") {
-            dropdownContainer.innerHTML = "";
-        }
-    }, 1000);
-});
-
-
-
-// ADD EVENT LISTENER WHEN THE SEARCH BUTTON IS CLICKED 
-// const searchButton = document.getElementById('search-button');
-
-// searchButton.addEventListener("click", () => {
-
-// });
-
-
-
-// CLEANER LA LISTE 
-// function clearList(){ 
-//     // looping through each child of the search results list and remove each child
-//     while (list.firstChild){
-//         list.removeChild(list.firstChild);
-//     }
-// }
-
 // AFFICHER UN MESSAGE D'ERREUR SI PAS DE RESULTAT 
 function noResults(){
     const error = document.createElement('li');
@@ -112,19 +91,4 @@ function noResults(){
     const text = document.createTextNode('No results found.');
     error.appendChild(text);
     list.appendChild(error);
-}
-
-
-// ADD EVENT LISTENER WHEN INPUT IS TYPED INTO THE INPUT FORM
-
-// searchInput.addEventListener('input', (e) => {
-//         if (searchInput && searchInput.trim().length > 2){ // 2. check if input exists and if input is larger than 2
-//             value = value.trim() // 3. redefine 'value' to exclude white space and change input to all lowercase
-//             setList(matchingBooks.filter(book => { 
-//                 // 4. returning only the results of setList() if the value of the search exists
-//                 return book.title.includes(value);
-//             }))
-//         } else {
-//             clearList()// 5. return nothing 
-//         }
-// });
+};
